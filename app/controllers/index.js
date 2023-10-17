36ba801a53519093845e254783a9c9c9ad1b5b42
romiari <romiari777@gmail.com>
2023-10-17 19:55:34 +0700

    first commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,8 @@
 const userCtrl = require('./userCtrl')
+<<<<<<< HEAD
 const course = require('./course')
 module.exports = {userCtrl, course}
+=======
+
+module.exports = {userCtrl}
+>>>>>>> e5f0d0e (first commit)
--- config/routes.js
+++ config/routes.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const multer = require('./fileHandler')
 const {userCtrl} = require('../app/controllers/index')
+<<<<<<< HEAD
 const {courses} = require('..app/controllers/index')
 
+=======
+>>>>>>> e5f0d0e (first commit)
 
 //user Routes
 router.get('/user-list', userCtrl.listUserHandle)
@@ -12,6 +15,7 @@ router.post('/user', userCtrl.createUserHandle)
 router.put('/user/:id', userCtrl.updateUserHandle)
 router.delete('/user/:id', userCtrl.deleteUserHandle)
 
+<<<<<<< HEAD
 //courses routes
 router.get('/course/:id', courses.getCourses)
 router.post('/course', courses.createCourse)
@@ -19,3 +23,6 @@ router.put('/course/:id', courses.updateCourse)
 router.delete('/course/:id', courses.deleteCourse)
 
 module.exports = router
+=======
+module.exports = router
+>>>>>>> e5f0d0e (first commit)
