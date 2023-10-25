2868e6c0ad3015aeda4ddce2dbedda8202570282
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-10-25 20:48:06 +0700

    fix: course model object format

--- app/models/courseModel.js
+++ app/models/courseModel.js
@@ -6,7 +6,6 @@ const courseSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-<<<<<<< HEAD
   description: {
     type: String,
     required: true,
@@ -20,21 +19,6 @@ const courseSchema = new mongoose.Schema({
     required: true,
   },
   enroll: {
-=======
-  description{
-    type: String,
-    required: true,
-  },
-  img{
-    type: String,
-    required: true,
-  },
-  instructor{
-    type: String,
-    required: true,
-  },
-  enroll{
->>>>>>> 088a13a (adding routes)
     type: boolean,
     required: true,
     default: false,
