220a76340d904f47f4cb0b16a03b28c3273b06dd
romiari <romiari777@gmail.com>
2023-10-26 17:35:27 +0700

    update userSvc, userCtrl, loginSvc, loginCtrl

--- app/controllers/loginCtrl.js
+++ app/controllers/loginCtrl.js
@@ -4,13 +4,14 @@ function loginHandle(req, res) {
     userLogin(req).then(data => {
         if (data.response) {
             res.status(data.response).json({
-                msg: data.message,
+                status: data.status,
+                message: data.message,
                 error: data.error
             })
         } else {
             res.status(200).json({
                 status: "OK",
-                msg:"Successful login",
+                message:"Successful login",
                 data
             })
         }
